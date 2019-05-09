a3e9c4f0623d26dec89f87ffe12848e837b15d4d
m.fukunishi <kani.crap@gmail.com>
2019-05-09 07:45:08 +0000

    CRUD messages

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+
+	<h1>品名の新規登録ページ</h1>
+
+    <div class="row">
+        <div class="col-6">
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
+                <div class="form-group">
+                    {!! Form::label('content', '品名:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+
+                {!! Form::submit('登録', ['class' => 'btn btn-primary']) !!}
+            {!! Form::close() !!}
+        </div>
+    </div>
+
+@endsection
