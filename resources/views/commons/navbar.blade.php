6a6f4983c5ed6a078538830edccffe33d1a53f71
m.fukunishi <kani.crap@gmail.com>
2019-05-21 08:59:18 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -10,7 +10,9 @@
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
                 <!--<li class="nav-item">{!! link_to_route('tasks.create', '新規品名の登録', [], ['class' => 'nav-link']) !!}</li>-->
-                <li class="nav-item"><a href="tasks/create" class="nav-link"><i class="far fa-plus-square mr-2"></i>新規品物の登録</a></li>
+                <!--<li class="nav-item"><a href="tasks/create" class="nav-link"><i class="far fa-plus-square mr-2"></i>新規品物の登録</a></li>-->
+                <li class="nav-item"><a href="/signup" class="nav-link"><i class="fas fa-user-plus mr-2"></i>新規ユーザー登録</a></li>
+                <li class="nav-item"><a href="/signup" class="nav-link"><i class="fas fa-lock mr-2"></i>ログイン</a></li>
             </ul>
         </div>
     </nav>
