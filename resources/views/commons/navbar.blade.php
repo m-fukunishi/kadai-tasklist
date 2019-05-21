f8c09ed5ff9f2e09e695c8d12e3de79a55d7c0db
m.fukunishi <kani.crap@gmail.com>
2019-05-21 09:20:55 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -9,10 +9,20 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                <!--<li class="nav-item">{!! link_to_route('tasks.create', '新規品名の登録', [], ['class' => 'nav-link']) !!}</li>-->
-                <!--<li class="nav-item"><a href="tasks/create" class="nav-link"><i class="far fa-plus-square mr-2"></i>新規品物の登録</a></li>-->
-                <li class="nav-item"><a href="/signup" class="nav-link"><i class="fas fa-user-plus mr-2"></i>新規ユーザー登録</a></li>
-                <li class="nav-item"><a href="/signup" class="nav-link"><i class="fas fa-lock mr-2"></i>ログイン</a></li>
+                @if (Auth::check())
+                    <!--<li class="nav-item"><a href="#" class="nav-link">メニュー</a></li>-->
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}さん</a>
+                        <ul class="dropdown-menu dropdown-menu-right">
+                            <li class="dropdown-item"><a href="tasks/create" class="nav-link"><i class="far fa-plus-square mr-2"></i>新規品物の登録</a></li>
+                            <li class="dropdown-divider"></li>
+                            <li class="dropdown-item">{!! link_to_route('logout.get', 'ログアウト') !!}</li>
+                        </ul>
+                    </li>
+                @else
+                    <li class="nav-item"><a href="/signup" class="nav-link"><i class="fas fa-user-plus mr-2"></i>新規ユーザー登録</a></li>
+                    <li class="nav-item"><a href="/login" class="nav-link"><i class="fas fa-lock mr-2"></i>ログイン</a></li>
+                @endif
             </ul>
         </div>
     </nav>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,29 +2,39 @@
 
 @section('content')
 
-    <h1 class="text-white mb-4"><i class="fas fa-list-alt mr-2"></i>買い物リスト一覧</h1>
+    @if (Auth::check())
+        <h1 class="text-white mb-4"><i class="fas fa-list-alt mr-2"></i>買い物リスト一覧</h1>
 
-    @if (count($tasks) > 0)
-        <table class="table table-sm bg-white">
-            <thead class="thead-light">
-                <tr>
-                    <th>id</th>
-                    <th>ステータス</th>
-                    <th>品物</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($tasks as $task)
-                <tr>
-                    <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
-                    <td>{{ $task->status }}</td>
-                    <td>{{ $task->content }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+        @if (count($tasks) > 0)
+            <table class="table table-sm bg-white">
+                <thead class="thead-light">
+                    <tr>
+                        <th>id</th>
+                        <th>ステータス</th>
+                        <th>品物</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($tasks as $task)
+                    <tr>
+                        <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                        <td>{{ $task->status }}</td>
+                        <td>{{ $task->content }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @endif
+
+        <p class="text-center mb-0">{!! link_to_route('tasks.create', '新規品物の登録', null, ['class' => 'btn btn-primary', 'style' => 'width:10em;']) !!}</p>
+
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>買い物リストにようこそ！</h1>
+                {!! link_to_route('signup.get', '新規ユーザー登録', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
+        </div>
     @endif
-
-    <p class="text-center mb-0">{!! link_to_route('tasks.create', '新規品物の登録', null, ['class' => 'btn btn-primary', 'style' => 'width:10em;']) !!}</p>
 
 @endsection
